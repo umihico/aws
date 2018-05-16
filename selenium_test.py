043d7ae0ed1ccd1efb8e294eb4d3907f94f3afb6
umihico <umihico_dummy@users.noreply.github.com>
2018-05-16 11:59:31 +0900

    submit selenium test script

---
+++ selenium_test.py
@@ -0,0 +1,13 @@
+"""first selenium test on aws ec2."""
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1280,1024')
+driver = webdriver.Chrome(chrome_options=options)
+driver.get('https://www.google.co.jp/')
+driver.save_screenshot('test.png')
