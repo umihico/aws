8687a0753de0f8fe3f099169f51c6e076e95c27b
umihico <umihico_dummy@users.noreply.github.com>
2018-06-05 10:58:40 +0900

    test multi pics

--- cw/google_cloud_vision_api/recognize_captcha.py
+++ cw/google_cloud_vision_api/recognize_captcha.py
@@ -3,68 +3,47 @@ import ast
 import base64
 import json
 from requests import Request, Session
-from GitHub.passpacker import passwords
+from passpacker import passwords
 
 
-def recognize_captcha(str_image_path):
-    bin_captcha = open(str_image_path, 'rb').read()
-    str_encode_file = base64.b64encode(bin_captcha).decode("UTF-8")
-
-    str_url = "https://vision.googleapis.com/v1/images:annotate?key="
-    # str_api_key = passwords['google_cloud_vision_api']
-    str_api_key = passwords['google_cloud_vision_api']
-    str_headers = {'Content-Type': 'application/json'}
-    str_json_data = {
-        'requests': [
-            {
-                'image': {
-                    'content': str_encode_file
-                },
-                'features': [
-                    {
-                        'type': "TEXT_DETECTION",
-                        'maxResults': 10
-                    }
-                ]
-            }
-        ]
-    }
+def recognize_captcha(image_path_list):
+    encode_files = [base64.b64encode(
+        open(path, 'rb').read()).decode("UTF-8") for path in image_path_list]
+    request_list = [
+        {'image': {'content': ef}, 'features': [
+            {'type': "TEXT_DETECTION", 'maxResults': 100}]}
+        for ef in encode_files]
+    json_data = json.dumps({'requests': request_list})
+    url = "https://vision.googleapis.com/v1/images:annotate?key="
+    api_key = passwords['google_cloud_vision_api']
+    headers = {'Content-Type': 'application/json'}
 
     print("Google Cloud Vision Api...", end='')
     obj_session = Session()
-    obj_request = Request("POST",
-                          str_url + str_api_key,
-                          data=json.dumps(str_json_data),
-                          headers=str_headers
-                          )
+    obj_request = Request("POST", url + api_key,
+                          data=json_data, headers=headers)
     obj_prepped = obj_session.prepare_request(obj_request)
-    obj_response = obj_session.send(obj_prepped,
-                                    verify=True,
-                                    timeout=60
-                                    )
+    obj_response = obj_session.send(obj_prepped, verify=True, timeout=60)
     print("end request")
-
-    if obj_response.status_code == 200:
-        # print(obj_response.text)
-        # with open('data.json', 'w') as outfile:
-        #     json.dump(obj_response.text, outfile)
-        try:
-            text = ast.literal_eval(obj_response.text)[
-                "responses"][0]["textAnnotations"][0]['description']
-        except (Exception, ) as e:
-            print(obj_response.text)
-            return 'error'
-        return text
+    try:
+        obj_response.raise_for_status()
+        dumped = ast.literal_eval(obj_response.text)
+        texts = []
+        for response in dumped["responses"]:
+            try:
+                text = response["textAnnotations"][0]['description']
+            except (IndexError, ) as e:
+                texts.append("IndexError")
+            else:
+                texts.append(text)
+        return texts
+    except (Exception, ) as e:
+        return obj_response.text
         # return obj_response.text
-    else:
-        print(obj_response.status_code)
-        print(obj_response)
-        print(obj_response.text)
-        return "error"
 
 
 if __name__ == '__main__':
-    textb = recognize_captcha("test.jpg")
+    text = recognize_captcha(["test.jpg", "test2.png"])
     print(text)
     # recognize_captcha("sosiki_tate.png")
     # load_data()
